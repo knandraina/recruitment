8d2ed423beb4bc0b6f4403b0d13406535723bac1
knandraina <kevin.rabesaotra@gmail.com>
2022-08-23 11:50:59 +0200

    approval removed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,27 +9,27 @@ import Page from '../components/Page/Page'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const loadSegment = () => {
-    const options = {
-      apiKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
-      page: true
-    }
-    if (process.env.NEXT_PUBLIC_NODE_ENV) {
-      return snippet.max(options)
-    } else {
-      return snippet.min(options)
-    }
-  }
+  // const loadSegment = () => {
+  //   const options = {
+  //     apiKey: process.env.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
+  //     page: true
+  //   }
+  //   if (process.env.NEXT_PUBLIC_NODE_ENV) {
+  //     return snippet.max(options)
+  //   } else {
+  //     return snippet.min(options)
+  //   }
+  // }
 
 
   return (
 
 
     <>
-      <Script
+      {/* <Script
         dangerouslySetInnerHTML={{ __html: loadSegment() }}
         id="segmentScript"
-      />
+      /> */}
       <Page>
         <Component {...pageProps} />
       </Page>
