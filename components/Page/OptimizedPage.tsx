06510bdfdc0c47c88bfcabc3a58823f7ca0b5204
knandraina <kevin.rabesaotra@gmail.com>
2022-08-30 17:31:22 +0200

    still for SEO and better text

--- components/Page/OptimizedPage.tsx
+++ components/Page/OptimizedPage.tsx
@@ -7,23 +7,25 @@ interface OptimizedPageProps {
 const Page = (props: OptimizedPageProps) => {
     return (
         <>
-            <div className="grid grid-cols-1 gap-4 px-4">
+            <div className="grid grid-cols-1 gap-4 px-4 sm:px-6 lg:px-8">
                 <div>
                     <h1 className="text-2xl tracking-tight font-bold text-blue-grey-800 sm:text-4xl md:text-3xl mt-4">
-                        <span className="block xl:inline">Software engineer salaries in {props.area}</span>{' '}
+                        <span className="block xl:inline">Software engineer salary in {props.area}</span>{' '}
                     </h1>
+                    <p className="text-s text-blue-grey-700 mt-2">How much does a Software Engineer make in the {props.area}?</p>
                 </div>
             </div>
-            <div className="grid grid-cols-2 gap-4 px-4 content-center">
+            <div className="grid grid-cols-2 px-4 content-center">
                 <div>
-                    <p className="text-blue-grey-300 mt-2 text-center">Average revenue</p>
+                    <p className="text-blue-grey-300 mt-2 text-center">Average salary</p>
                     <p className="text-2xl text-light-blue-400 text-center">{props.compensation} €</p>
                 </div>
                 <div>
-                    <p className="text-blue-grey-300 mt-2 text-center">Median revenue</p>
+                    <p className="text-blue-grey-300 mt-2 text-center">Median salary</p>
                     <p className="text-2xl text-light-blue-400 text-center">{props.median} €</p>
                 </div>
             </div>
+            <p className="px-4 sm:px-6 lg:px-8 mt-2 text-s text-blue-grey-700">{`The average salary for software engineer in ${props.area} is ${props.compensation} €, and the median salary is ${props.median} €`}</p>
         </>
     )
 }
