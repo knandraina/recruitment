c4d0a581a8f7cf785bba8b7c85645241b4df50dd
knandraina <kevin.rabesaotra@gmail.com>
2022-08-30 20:57:42 +0200

    new link

--- components/Page/OptimizedPage.tsx
+++ components/Page/OptimizedPage.tsx
@@ -2,6 +2,7 @@ interface OptimizedPageProps {
     area: string,
     compensation: number,
     median: number,
+    role?: string
 }
 
 const Page = (props: OptimizedPageProps) => {
@@ -10,9 +11,9 @@ const Page = (props: OptimizedPageProps) => {
             <div className="grid grid-cols-1 gap-4 px-4 sm:px-6 lg:px-8">
                 <div>
                     <h1 className="text-2xl tracking-tight font-bold text-blue-grey-800 sm:text-4xl md:text-3xl mt-4">
-                        <span className="block xl:inline">Software engineer salary in {props.area}</span>{' '}
+                        <span className="block xl:inline">{`${props.role ? props.role :'Software Engineer'} salary in ${props.area}`}</span>{' '}
                     </h1>
-                    <p className="text-s text-blue-grey-700 mt-2">How much does a Software Engineer make in the {props.area}?</p>
+                    <p className="text-s text-blue-grey-700 mt-2">{`How much does a ${props.role ? props.role :'Software Engineer'} make in the ${props.area}?`}</p>
                 </div>
             </div>
             <div className="grid grid-cols-2 px-4 content-center">
@@ -25,7 +26,7 @@ const Page = (props: OptimizedPageProps) => {
                     <p className="text-2xl text-light-blue-400 text-center">{props.median} €</p>
                 </div>
             </div>
-            <p className="px-4 sm:px-6 lg:px-8 mt-2 text-s text-blue-grey-700">{`The average salary for software engineer in ${props.area} is ${props.compensation} €, and the median salary is ${props.median} €`}</p>
+            <p className="px-4 sm:px-6 lg:px-8 mt-2 text-s text-blue-grey-700">{`The average salary for ${props.role ? props.role :'Software Engineer'} in ${props.area} is ${props.compensation} €, and the median salary is ${props.median} €`}</p>
         </>
     )
 }
---
+++ pages/salaries/[country]/[department]/[role]/index.tsx
@@ -0,0 +1,72 @@
+import { GetStaticProps, GetStaticPaths } from 'next'
+import { useState } from 'react';
+
+import { loadData, loadDepartmentData } from '../../../../../lib/load-data'
+import connectionDB from '../../../../../lib/connectionDB'
+import { metricsCompensation } from '../../../../../lib/calculation';
+
+import Table from '../../../../../components/Table/table';
+import OptimizedPage from '../../../../../components/Page/OptimizedPage'
+import FormRedirection from '../../../../../components/Form/FormRedirection';
+import Footer from '../../../../../components/Element/Footer';
+
+import { NextSeo } from 'next-seo';
+
+
+// `getStaticPaths` requires using `getStaticProps`
+export const getStaticProps: GetStaticProps = async (context: any) => {
+    const country: String = context.params.country;
+    const department_lower_case: String = context.params.department;
+    const role: string = context.params.role
+    await connectionDB();
+    const response = await loadData(department_lower_case, role);
+    const department = await response[0].department
+    const { meanCompensation, medianCompensation } = await metricsCompensation(response)
+
+    return {
+        // Passed to the page component as props
+        props: {
+            post: response,
+            country,
+            department,
+            role,
+            compensation: Math.round(meanCompensation),
+            median: Math.round(medianCompensation)
+        },
+    }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+
+    await connectionDB();
+    const path: Array<any> = await loadDepartmentData()
+    return {
+        paths: path,
+        fallback: false, // can also be true or 'blocking'
+    }
+}
+
+
+const RoleData = (props: any) => {
+
+    const [department, setDepartment] = useState<string>(props.department);
+
+    const handleChange = async (department: string) => {
+        setDepartment(department)
+    }
+
+    return (
+        <>
+            <NextSeo
+                title={`Discover ${props.role} salaries in ${props.country}`}
+                description={`Leverage our database to know the sofware engineer wage in ${props.country}`}
+            />
+            <OptimizedPage area={props.department} compensation={props.compensation} median={props.median} role={props.role}/>
+            <Table compensation={props} department={props.department} role={props.role}/>
+            <FormRedirection department={department} handleChange={handleChange} textButton={"Explore Data"} />
+            <Footer />
+        </>
+    )
+}
+
+export default RoleData;
