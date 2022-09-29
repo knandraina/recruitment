0ebf52f3bff3cfa1ce574121e5b1616f72f2f51b
knandraina <kevin.rabesaotra@gmail.com>
2022-09-29 16:30:28 +0200

    analytics

--- next.config.js
+++ next.config.js
@@ -1,3 +1,29 @@
+let securityHeaders = []
+
+const ContentSecurityPolicy = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.segment.com;
+    child-src *.youtube.com *.google.com *.twitter.com *.segment.com;
+    frame-src *.segment.com;
+    style-src 'self' 'unsafe-inline' *.googleapis.com;
+    img-src * blob: data:;
+    media-src 'none';
+    connect-src *;
+    font-src 'self';
+`;
+
+securityHeaders = ContentSecurityPolicy
+
+
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: [{key: 'Content-Security-Policy', value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim()}],
+      },
+    ]
+  },
 }
