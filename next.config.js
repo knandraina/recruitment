f49f63206dd97318244138360b450db5ece8f1f2
knandraina <kevin.rabesaotra@gmail.com>
2022-09-30 17:08:00 +0200

    netlify csp frame

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ let securityHeaders = []
 const ContentSecurityPolicy = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.segment.com ;
-    script-src-elem https://netlify-cdp-loader.netlify.app/netlify.js;
+    script-src-elem https://*.netlify.app/*;
     connect-src 'self' https://cdn.segment.com/v1/projects/PzoD1qlC1wpvDGhNckresPQM3zcX8I1s/settings;
     frame-src https://app.netlify.com/;
     child-src *.youtube.com *.google.com *.twitter.com *.segment.com;
