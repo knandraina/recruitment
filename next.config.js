e9eb519e2936591e57fe90943a9788b3a9e11030
knandraina <kevin.rabesaotra@gmail.com>
2022-10-03 13:28:00 +0200

    csp error 10

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const ContentSecurityPolicy = `
     script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.segment.com ;
     script-src-elem *;
     connect-src *;
+    report-uri https://cdn.segment.com/v1/projects/PzoD1qlC1wpvDGhNckresPQM3zcX8I1s/settings;
     frame-src https://app.netlify.com/;
     child-src *.youtube.com *.google.com *.twitter.com *.segment.com;
     style-src 'self' 'unsafe-inline' *.googleapis.com;
