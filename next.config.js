f1b6a47cf008dff3e52b1f5afeccb441a8b35984
knandraina <kevin.rabesaotra@gmail.com>
2022-10-03 13:11:03 +0200

    csp error 7

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const ContentSecurityPolicy = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com *.segment.com ;
     script-src-elem *;
-    connect-src 'self' *.segment.io https://cdn.segment.com/v1/projects/l9AnFLhpWlu8mt67R6LRR49PH9e8wOPq/settings https://cdn.segment.com/v1/projects/PzoD1qlC1wpvDGhNckresPQM3zcX8I1s/settings;
+    connect-src 'unsafe-inline' *.segment.io https://cdn.segment.com/v1/projects/l9AnFLhpWlu8mt67R6LRR49PH9e8wOPq/settings https://cdn.segment.com/v1/projects/PzoD1qlC1wpvDGhNckresPQM3zcX8I1s/settings;
     frame-src https://app.netlify.com/;
     child-src *.youtube.com *.google.com *.twitter.com *.segment.com;
     style-src 'self' 'unsafe-inline' *.googleapis.com;
