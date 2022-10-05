c1c77bc920dea7a0d1be000930a8eaa617847b31
knandraina <kevin.rabesaotra@gmail.com>
2022-10-05 16:54:49 +0200

    all role will have seo function

--- pages/api/new-compensation/index.ts
+++ pages/api/new-compensation/index.ts
@@ -55,7 +55,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             product_feedback
         } = req.body.compensation
 
-
+        const seo = await seoFunction(role); 
 
         const response = await addNewCompensation(
             revenue,
@@ -76,7 +76,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             industry,
             anonymous,
             feedback_job_form,
-            solution_tested)
+            solution_tested, seo)
 
 
         await analytics.track({
@@ -150,7 +150,8 @@ async function addNewCompensation(
     industry: string,
     anonymous: string,
     feedback_job_form: string,
-    solution_tested: any) {
+    solution_tested: any,
+    seo: any) {
     const company = await createCompany(name,
         company_size,
         company_stage,
@@ -171,6 +172,7 @@ async function addNewCompensation(
         role,
         anonymous,
         technology_used,
+        seo,
     );
     
 
@@ -220,6 +222,7 @@ async function createCompensation(
     role: string,
     anonymous: string,
     technology_used: any,
+    seo: any,
 ) {
 
     const compensation = await Compensation.create({
@@ -239,9 +242,52 @@ async function createCompensation(
         company: company._id,
         department: undefined,
         department_lower_case: undefined,
-        category_role: role
+        category_role: role,
+        seo: seo
     })
     return compensation;
+}
+
+const seoFunction = async (role: string) => {
+    if (role === 'Software Engineer, Full-Stack') {
+        return ['Full-Stack Developer', 'Full-Stack Engineer', "A full-stack engineer is a position in high demand these days. This role requires general knowledge of all the different layers of software development. At a minimum, he/she must have functional knowledge of the front-end and back-end of software development, which they can turn into a finished project. In some cases, depending on the company and its requirements,a full-stack engineer is expected to have extensive knowledge and technical expertise in these different areas."]
+    } else if (role === 'Software Engineer, Front-end') {
+        return ['Front-end Engineer', 'Front-end Developer', 'A front-end engineer is vital to the success of a website. The position of the front-end engineer focuses on making websites friendly and functional to their users. So, no matter how great the product offering on the website is or how interesting the stories are, much of the utility of it depends on the users ability to navigate it. The front-end engineer makes websites user-friendly by creating rich and exciting features such as animations, text boxes, games, forums, and even navigation. They often coordinate their efforts with the back-end engineers who then process the data.']
+    } else if (role === 'Software Engineer, Back-end') {
+        return ['Back-end Engineer','Back-end Developer', 'Back-end engineers rely on their knowledge of Java, C++, .NET, C# and other skills to create data access layers for software and applications. Back-end engineers also analyze, troubleshoot, and diagnose system hardware and physical infrastructure to maintain optimal performance. Back-end engineers provide valuable services by writing code that enables browsers and databases to communicate effectively.']
+    } else if (role === 'Software Engineer, Mobile') {
+        return ['Mobile Developer','Mobile Engineer', 'Back-end engineers rely on their knowledge of Java, C++, .NET, C# and other skills to create data access layers for software and applications. Back-end engineers also analyze, troubleshoot, and diagnose system hardware and physical infrastructure to maintain optimal performance. Back-end engineers provide valuable services by writing code that enables browsers and databases to communicate effectively.']
+    } else if (role === 'Software Engineer, Embedded Systems') {
+        return ['Embedded Systems Engineer', 'Embedded Systems Developer', 'An embedded engineer works with the code of dedicated computer systems within larger electrical and mechanical systems. They create code that addresses and solves problems within the system for proper functioning. Embedded engineers also help develop systems and provide technical support directly to users within an organization.']
+    } else if (role === 'DevOps') {
+        return ['DevOps Engineer', 'Platform Engineer', 'DevOps engineers often focus on automating deployment and network operations. At many organizations, they are responsible for creating and maintaining development platforms and processes that enable automation of builds and releases.']
+    } else if (role === 'AdminSys') {
+        return ['Systems Administrator','Administrator Systems','Systems administrators are also known as SysAdmins. They maintain the computer systems of organizations to ensure reliable operation. Systems administrators handle backup procedures, software upgrades, and patches. They perform security monitoring to make sure a computer system is running smoothly. SysAdmins understand network services, resource requirements for applications, and access controls.']
+    } else if (role === 'Data Analyst') {
+        return ['Data Analyst', 'Data Analyst', 'Data analysts synthesize and derive learnings from data to inform and drive business strategy. Their duties may include identifying areas to increase efficiency and automation of processes, setting up and maintaining automated data processes, producing and tracking key performance indicators, monitoring and auditing data quality, and creating dashboards, graphs, and visualisations.']
+    } else if (role === 'Data Scientist') {
+        return ['Data Scientist', 'Data Scientist', 'A data scientist is often well versed in statistics and machine learning. They are often hired to solve complex problems related to prediction, recommendations, pattern detection, automated processes, decision-making, and scoring and ranking.']
+    } else if (role === 'Site Reliability Engineer') {
+        return ['SRE','Site Reliability Engineer', 'Software architects are responsible for mapping the technical requirements to business needs and ensuring they meet the architectural demands of the organization. They handle all systems solutions, including those geared towards human resources, supply chains, product life cycles, and customer relations.']
+    } else if (role === 'Software Architect') {
+        return ['Software Architect', 'Software Architect', 'Software architects are responsible for mapping the technical requirements to business needs and ensuring they meet the architectural demands of the organization. They handle all systems solutions, including those geared towards human resources, supply chains, product life cycles, and customer relations.']
+    } else if (role === 'Software Security Engineer') {
+        return ['Security Engineer', 'Security Developer', 'Security engineers maintain the security of computer systems and networks within an organization. They create and implement strategies to prevent unauthorized internal network access and protect sensitive information. Also known as information security analysts, these engineers work with IT teams to develop policies and plans for information security. They create best practices and security standards that enhance system protection and teach others in the company to follow them.']
+    } else if (role === 'Software Network Engineer') {
+        return ['Network Engineer', 'Network Developer', 'A networking software engineer specializes in designing, building and installing software for existing or new networks and infrastructures. Often, organizations require bespoke software solutions. By writing and creating these platforms, networking software engineers allow organizations to operate more efficiently, maximize their productivity and function securely.']
+    } else if (role === 'QA Engineer') {
+        return ['QA automation engineers', 'QA Engineer', 'QA automation engineers are generally responsible for writing and conducting automated tests to verify software or apps end-user quality and the structural integrity of back-end databases. The role of a QA automation engineer often includes duties like designing, creating, and implementing tests, developing automation strategies, overseeing the execution of automated scripts, and training junior members of the automation team']
+    } else if (role === 'Data Engineer') {
+        return ['Data Engineer', 'Data Engineer', 'Data engineers create data pipelines for organizational analysis. Candidates are responsible for extracting and transferring big data, then improving and presenting that information to data scientists and other team members. They solve problems with data integration and must often work with unstructured data sets to make them easier to understand. They help develop and construct databases and large-scale data processing systems and test and maintain these systems for proper functionality.']
+    } else if (role === 'Machine Learning Engineer') {
+        return ['Machine Learning Engineer', 'Machine Learning Engineer', 'The machine learning engineer position describes an engineer working with one of the various machine learning models. A machine learning engineer develops the infrastructure to support the model. This may be on a large or small scale, depending on the company and the scope of work. To work in artificial intelligence (AI), a machine learning engineer should have a solid background in data science, applied research, and coding.']
+    } else if (role === 'Principal Engineer') {
+        return ['Principal Engineer', 'Principal Engineer', 'A principal engineer is an engineering professional with many years of experience in their field. They oversee projects and staff after working as engineers in a particular field. Different from other engineers, the role of principal engineer is a leadership role where they guide staff to ensure an engineering team completes projects on time and within budgets.']
+    } else if (role === 'Engineering Director') {
+        return ['Head of Engineering', 'President of Engineering', 'A director of engineering is a leader that oversees and guides a company&apos;s engineering department. Also known as the vice president of engineering or the head of engineering, these professionals help ensure that an organization&apos;s engineering goals align with the company s values, mission and goals. Often, these professionals involve themselves with administrative, financial and human resource tasks to help promote the success of the department.']
+    } else if (role === 'CTO') {
+        return ['Chief Technology Officer', 'CTO', 'A chief technology officer (CTO) is the executive in charge of an organization&apos;s technological needs as well as its research and development (R&D). Also known as a chief technical officer, this individual examines the short- and long-term needs of an organization and utilizes capital to make investments designed to help the organization reach its objectives. The CTO usually reports directly to a company&apos;s chief information officer (CIO), but may also report to the chief executive officer of the firm.']
+    }
 }
 
 export default connectDB(handler);
