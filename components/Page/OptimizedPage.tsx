d811422a4f04cfd76fa2dfc436decac4d0f27627
knandraina <kevin.rabesaotra@gmail.com>
2022-10-12 18:19:33 +0200

    detail

--- components/Page/OptimizedPage.tsx
+++ components/Page/OptimizedPage.tsx
@@ -27,9 +27,9 @@ const Page = (props: OptimizedPageProps) => {
             <div className="grid grid-cols-12 gap-4 px-4 sm:px-6 lg:px-8">
                 <div className="col-start-2 col-span-10">
                     <h1 className="text-2xl tracking-tight font-bold text-blue-grey-800 sm:text-4xl md:text-3xl mt-4">
-                        {props.locale === 'fr' ? 
-                        <span className="block xl:inline">{props.t('h1', {role: props.role ? props.seo.fr.first_role : 'Software Engineer', gender: props.gender ? `${props.gender}` : '', area: props.city_link_department ? ` à ${props.city_link_department}` : props.area ? `à ${props.area}` : `en ${props.country}` } )}</span> :
-                         <span className="block xl:inline">{props.t('h1', {role: props.role ? props.seo.en.first_role : 'Software Engineer', gender: props.gender ? `for ${props.gender}` : '', area: props.city_link_department ? props.city_link_department : props.area ? props.area : props.country } )}</span>}
+                        {props.locale === 'fr' ?
+                            <span className="block xl:inline">{props.t('h1', { role: props.role ? props.seo.fr.first_role : 'Software Engineer', gender: props.gender ? `${props.gender}` : '', area: props.city_link_department ? ` à ${props.city_link_department}` : props.area ? `à ${props.area}` : `en ${props.country}` })}</span> :
+                            <span className="block xl:inline">{props.t('h1', { role: props.role ? props.seo.en.first_role : 'Software Engineer', gender: props.gender ? `for ${props.gender}` : '', area: props.city_link_department ? props.city_link_department : props.area ? props.area : props.country })}</span>}
                     </h1>
                 </div>
             </div>
